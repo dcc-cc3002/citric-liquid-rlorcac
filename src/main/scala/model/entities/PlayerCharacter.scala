5df84c6cd3395119d1cc7d476db5bf496c3c310b
Raimundo Lorca Correa <89880069+rlorcac@users.noreply.github.com>
2023-09-10 00:31:31 -0300

    Create class WildUnit; update class PlayerCharacter

--- src/main/scala/model/entities/PlayerCharacter.scala
+++ src/main/scala/model/entities/PlayerCharacter.scala
@@ -43,9 +43,17 @@ class PlayerCharacter(val name: String,
               defense: Int,
               evasion: Int,
               val randomNumberGenerator: Random = new Random()) extends AbstractEntity(maxHp, attack, defense, evasion) {
+  var stars = 0
 
   /** Rolls a dice and returns a value between 1 to 6. */
   def rollDice(): Int = {
     randomNumberGenerator.nextInt(6) + 1
+  }
+
+  /** Called when the unit starts its turn
+   *
+   */
+  def startTurn(): Unit = {
+
   }
 }
