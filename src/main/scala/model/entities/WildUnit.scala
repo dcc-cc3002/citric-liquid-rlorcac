d924d20b0fd9631ea062b545b663ecb01442ca3b
Raimundo Lorca Correa <89880069+rlorcac@users.noreply.github.com>
2023-09-10 01:47:24 -0300

    make WildUnits start at 0 stars

--- src/main/scala/model/entities/WildUnit.scala
+++ src/main/scala/model/entities/WildUnit.scala
@@ -5,7 +5,7 @@ class WildUnit(maxHp: Int,
                attack: Int,
                defense: Int,
                evasion: Int,
-               var stars: Int) extends AbstractEntity(maxHp, attack, defense, evasion) {
+               var stars: Int = 0) extends AbstractEntity(maxHp, attack, defense, evasion) {
   /** Called when the unit starts its turn
    *
    *  For a WildUnit, this method does nothing.
