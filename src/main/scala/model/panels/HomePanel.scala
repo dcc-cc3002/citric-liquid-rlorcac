c94210c6d89f2f29df7b88c4aa64c80cbcdf7074
Raimundo Lorca Correa <89880069+rlorcac@users.noreply.github.com>
2023-09-10 00:28:30 -0300

    Create HomePanel.scala

---
+++ src/main/scala/model/panels/HomePanel.scala
@@ -0,0 +1,23 @@
+package cl.uchile.dcc.citric
+package model.panels
+
+import model.entities.PlayerCharacter
+
+/** Represents a panel that's home to a `PlayerCharacter`, where they can Norma Clear
+ *
+ * @param atHome The PlayerCharacter that has this panel set as their HomePanel
+ */
+class HomePanel(val atHome: PlayerCharacter) extends AbstractPanel{
+  /** To be called when a `PlayerCharacter` lands on a `Panel`
+   *
+   * @param player The `PlayerCharacter` that landed on the `Panel`
+   */
+  def activate(player: PlayerCharacter): Unit = {
+    /** Check if player can perform a normaClear
+     *
+     */
+    if(player == atHome){
+      // Here goes NormaClear logic
+    }
+  }
+}
