10ed93cdcd1006000267a1845255ed983bc9f62d
Raimundo Lorca Correa <89880069+rlorcac@users.noreply.github.com>
2023-09-26 13:52:44 -0300

    doc(Entity)

--- src/main/scala/model/entities/Entity.scala
+++ src/main/scala/model/entities/Entity.scala
@@ -22,21 +22,21 @@ trait Entity {
 
   /** Adds HP up to the maximum allowed HP
    *
-   * @param dHp Max amount of HP to add
+   * @param dHp Max amount of HP to add. Must be non-negative.
    * @return Amount of HP actually added
    */
   def addHp(dHp: Int): Int
 
   /** Adds stars to the current star count
    *
-   * @param amount Amount of stars to add
+   * @param amount Amount of stars to add. Must be non-negative.
    * @return Amount of stars actually added
    */
   def addStars(amount: Int): Int
 
   /** Takes away HP until 0 is reached
    *
-   * @param dHp Max amount of HP to take away
+   * @param dHp Max amount of HP to take away. Must be non-negative.
    * @return Amount of HP actually taken away
    */
 
@@ -45,7 +45,7 @@ trait Entity {
 
   /** Takes away stars until 0 is reached
    *
-   * @param amount Max amount of stars to take away
+   * @param amount Max amount of stars to take away. Must be non-negative.
    * @return Amount of stars actually taken away
    */
   def deductStars(amount: Int): Int
