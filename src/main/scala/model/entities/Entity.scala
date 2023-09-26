dc4855c6c71b61b86e31743335d99b2cc7192ab0
Raimundo Lorca Correa <89880069+rlorcac@users.noreply.github.com>
2023-09-26 13:17:25 -0300

    refactor(model.entities): move common methods up to Entity

--- src/main/scala/model/entities/Entity.scala
+++ src/main/scala/model/entities/Entity.scala
@@ -6,13 +6,50 @@ trait Entity {
   val attack: Int
   val defense: Int
   val evasion: Int
-  var stars: Int
 
   /**
    *
    * @return Current HP of the `Entity`
    */
   def getCurrentHp: Int
+
+  /**
+   *
+   * @return Current star count of the `Entity`
+   */
+  def getStars: Int
+
+  /** Adds HP up to the maximum allowed HP
+   *
+   * @param dHp Max amount of HP to add
+   * @return Amount of HP actually added
+   */
+  def addHp(dHp: Int): Int
+
+  /** Adds stars to the current star count
+   *
+   * @param amount Amount of stars to add
+   * @return Amount of stars actually added
+   */
+  def addStars(amount: Int): Int
+
+  /** Takes away HP until 0 is reached
+   *
+   * @param dHp Max amount of HP to take away
+   * @return Amount of HP actually taken away
+   */
+
+  def deductHp(dHp: Int): Int
+
+
+  /** Takes away stars until 0 is reached
+   *
+   * @param amount Max amount of stars to take away
+   * @return Amount of stars actually taken away
+   */
+  def deductStars(amount: Int): Int
+
+
   /** Called when the unit starts its turn
    *
    */
