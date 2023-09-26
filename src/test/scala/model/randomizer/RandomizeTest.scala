a737b3f05f7c7eef1391254b50545befaafe5a24
Raimundo Lorca Correa <89880069+rlorcac@users.noreply.github.com>
2023-09-26 15:01:47 -0300

    test(model.randomizer)

---
+++ src/test/scala/model/randomizer/RandomizeTest.scala
@@ -0,0 +1,21 @@
+package cl.uchile.dcc.citric
+package model.randomizer
+
+import scala.util.Random
+
+class RandomizeTest extends munit.FunSuite {
+  private var rand: Randomize = _
+  private var rng: Random = _
+
+  override def beforeEach(context: BeforeEach): Unit = {
+    rand = new Randomize()
+    rng = new Random(11)
+  }
+
+  test("Generated WildUnits should be in the allowed list"){
+    for(_ <- 1 to 10){
+      assert(rand.getWildUnitList.contains(rand.randomUnit()))
+    }
+  }
+
+}
