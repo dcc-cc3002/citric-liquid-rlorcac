91d3438a275a44b838da85da74055addc5241014
Raimundo Lorca Correa <89880069+rlorcac@users.noreply.github.com>
2023-09-26 13:53:06 -0300

    feat(AbstractEntity)!: check that changes are non-negative

--- src/main/scala/model/entities/AbstractEntity.scala
+++ src/main/scala/model/entities/AbstractEntity.scala
@@ -11,12 +11,18 @@ abstract class AbstractEntity(val maxHp: Int,
   def getStars: Int = stars
 
   def addStars(amount: Int): Int = {
+    if(amount < 0) {
+      return 0
+    }
     val originalStars: Int = stars
     stars = stars + amount
     stars - originalStars
   }
 
   def addHp(dHp:  Int): Int = {
+    if (dHp < 0) {
+      return 0
+    }
     val originalHp = currentHp
     currentHp = maxHp.min(currentHp + dHp)
     currentHp - originalHp
@@ -24,12 +30,18 @@ abstract class AbstractEntity(val maxHp: Int,
 
 
   def deductStars(amount: Int): Int = {
+    if (amount < 0) {
+      return 0
+    }
     val originalStars: Int = stars
     stars = 0.max(stars - amount)
     originalStars - stars
   }
 
   def deductHp(dHp: Int): Int = {
+    if (dHp < 0) {
+      return 0
+    }
     val originalHp: Int = currentHp
     currentHp = 0.max(currentHp - dHp)
     originalHp - currentHp
