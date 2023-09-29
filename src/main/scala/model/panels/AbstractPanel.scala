4b0454346386125a0f09a66e2ad5464ce07f9fa6
Raimundo Lorca Correa <89880069+rlorcac@users.noreply.github.com>
2023-09-29 01:52:52 -0300

    fix(AbstractPanel): was returning mutable object

--- src/main/scala/model/panels/AbstractPanel.scala
+++ src/main/scala/model/panels/AbstractPanel.scala
@@ -11,21 +11,21 @@ abstract class AbstractPanel extends Panel{
   private val _characters: ArrayBuffer[PlayerCharacter] = new ArrayBuffer[PlayerCharacter]
   private var _nextPanels: ArrayBuffer[Panel] = new ArrayBuffer[Panel]
 
-  override def characters: ArrayBuffer[PlayerCharacter] = _characters
+  override def characters: ArrayBuffer[PlayerCharacter] = _characters.clone()
 
-  override def nextPanels: ArrayBuffer[Panel] = _nextPanels
+  override def nextPanels: ArrayBuffer[Panel] = _nextPanels.clone()
 
   def nextPanels_=(newNext: ArrayBuffer[Panel]):Unit = {_nextPanels = newNext}
 
   def addCharacter(player: PlayerCharacter): Unit = {
-    characters.append(player)
+    _characters.append(player)
     activate(player)
   }
 
   def removeCharacter(player: PlayerCharacter): Unit = {
     if(!this.characters.contains(player)) throw new Exception("Player not found in requested panel")
     while(this.characters.contains(player)){
-      characters.remove(characters.indexOf(player))
+      _characters.remove(characters.indexOf(player))
     }
   }
 }
