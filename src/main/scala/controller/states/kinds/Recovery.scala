385c1c53487a8bbd37dfde5999315441614f437c
Raimundo Lorca Correa <89880069+rlorcac@users.noreply.github.com>
2023-11-27 01:29:50 -0300

    add(GameController, controller.states.kinds.*)
    A few of the necessary states to control the game's flow.

---
+++ src/main/scala/controller/states/kinds/Recovery.scala
@@ -0,0 +1,11 @@
+package cl.uchile.dcc.citric
+package controller.states.kinds
+
+import model.entities.Player
+
+import controller.GameController
+import controller.states.AbstractGameState
+
+class Recovery(controller: GameController, player: Player) extends AbstractGameState("Recovery", controller){
+
+}
