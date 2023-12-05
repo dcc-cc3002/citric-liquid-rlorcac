a833da222b714ed2327d4e2e53a759e78b287f1d
Raimundo Lorca Correa <89880069+rlorcac@users.noreply.github.com>
2023-12-05 13:12:17 -0300

    test Entity.DefeatEnemy

--- src/test/scala/model/entities/DefeatEnemyTest.scala
+++ src/test/scala/model/entities/DefeatEnemyTest.scala
@@ -1,12 +1,12 @@
 package cl.uchile.dcc.citric
 package model.entities
 
-import cl.uchile.dcc.citric.model.randomizer.RandomizedFactory
+import model.randomizer.{RandomWildUnitFactory, RandomizedFactory}
 import munit.FunSuite
 
 import scala.util.Random
 
-class DefeatedByTest extends FunSuite {
+class DefeatEnemyTest extends FunSuite {
   private var player1: Player = _
   private var player2: Player = _
   private var wildUnit1: WildUnit = _
@@ -18,6 +18,7 @@ class DefeatedByTest extends FunSuite {
   private val attack = 1
   private val defense = 1
   private val evasion = 1
+  private val wildUnitFactory = new RandomWildUnitFactory()
   override def beforeEach(context: BeforeEach): Unit = {
     randomNumberGenerator = new Random(13)
     player1 = new PlayerCharacter(name1,
@@ -36,14 +37,14 @@ class DefeatedByTest extends FunSuite {
       "Stars",
       randomNumberGenerator
     )
-    wildUnit1 = new RandomizedFactory().randomUnit()
-    wildUnit2 = new RandomizedFactory().randomUnit()
+    wildUnit1 = wildUnitFactory.create()
+    wildUnit2 = wildUnitFactory.create()
   }
 
   test("Player defeated by Player"){
     player1.addStars(2)
     player2.addStars(2)
-    player2.defeatedBy(player1)
+    player1.defeatEnemy(player2)
     assertEquals(player1.stars, 3)
     assertEquals(player1.victories, 2)
     assertEquals(player2.stars, 1)
@@ -53,7 +54,7 @@ class DefeatedByTest extends FunSuite {
   test("Player defeated by Wild Unit"){
     player1.addStars(2)
     val originalStars = wildUnit1.stars
-    player1.defeatedBy(wildUnit1)
+    wildUnit1.defeatEnemy(player1)
     assertEquals(player1.stars, 1)
     assertEquals(player1.victories, 0)
     assertEquals(wildUnit1.stars, originalStars + 1)
@@ -63,7 +64,7 @@ class DefeatedByTest extends FunSuite {
     player1.addStars(2)
     val originalStars = wildUnit1.stars
     println(originalStars)
-    wildUnit1.defeatedBy(player1)
+    player1.defeatEnemy(wildUnit1)
     assertEquals(player1.stars, 2 + originalStars)
     assertEquals(player1.victories, 1)
     assertEquals(wildUnit1.stars, 0)
@@ -74,7 +75,7 @@ class DefeatedByTest extends FunSuite {
     wildUnit2.addStars(5)
     val originalStars1 = wildUnit1.stars
     val originalStars2 = wildUnit2.stars
-    wildUnit2.defeatedBy(wildUnit1)
+    wildUnit1.defeatEnemy(wildUnit2)
     assertEquals(wildUnit1.stars, originalStars1)
     assertEquals(wildUnit2.stars, originalStars2)
   }
