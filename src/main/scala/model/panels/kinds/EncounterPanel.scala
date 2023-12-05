722eab57d1ec42e83fe07bb73102687f0f3dede4
Raimundo Lorca Correa <89880069+rlorcac@users.noreply.github.com>
2023-12-05 13:24:28 -0300

    add RandomWildUnitFactory to EncounterPanel

--- src/main/scala/model/panels/kinds/EncounterPanel.scala
+++ src/main/scala/model/panels/kinds/EncounterPanel.scala
@@ -3,22 +3,24 @@ package model.panels.kinds
 
 import model.entities.{Player, WildUnit}
 import model.panels.AbstractPanel
-import model.randomizer.RandomizedFactory
+import model.randomizer.{RandomWildUnitFactory, RandomizedFactory}
+
+import scala.util.Random
 
 /** Represents a panel where players fight a wild unit upon landing.
  *
  */
 class EncounterPanel extends AbstractPanel {
   private var wildUnit: Option[WildUnit] = None
+  private val wildUnitFactory: RandomWildUnitFactory = new RandomWildUnitFactory(new Random())
   /** To be called when a `PlayerCharacter` lands on a `Panel`
    *
    * @param player The `PlayerCharacter` that landed on the `Panel`
    */
   def activate(player: Player): Unit = {
     if(wildUnit.isEmpty){
-      wildUnit = Some(new RandomizedFactory().randomUnit())
+      wildUnit = Some(wildUnitFactory.create())
     }
-    // TODO: Combat goes here.
   }
 
   def getCurrentUnit: Option[WildUnit] = wildUnit
