3cc0f308523e775a080ddfe4b19a4d679dbceed1
Raimundo Lorca Correa <89880069+rlorcac@users.noreply.github.com>
2023-12-05 22:25:03 -0300

    add wild unit combat

--- src/main/scala/controller/states/kinds/WildUnitCombat.scala
+++ src/main/scala/controller/states/kinds/WildUnitCombat.scala
@@ -5,7 +5,21 @@ import controller.GameController
 import controller.states.AbstractGameState
 import model.entities.{Player, WildUnit}
 
-class WildUnitCombat(controller: GameController, initiator: Player, receiver: WildUnit) extends AbstractGameState("PlayerCombat", controller) {
+import scala.util.Random
+
+class WildUnitCombat(controller: GameController, initiator: Player, receiver: WildUnit) extends AbstractGameState("WildUnitCombat", controller) {
+  override def combatRound(): Unit = {
+    if (Random.nextInt(1) == 0) receiver.evadeRoll(initiator) else receiver.defendRoll(initiator)
+    if (receiver.currentHp == 0) {
+      initiator.defeatEnemy(receiver)
+    }
+    else {
+      if (context.getInput(List("Evade", "Defend")) == 0) initiator.evadeRoll(receiver) else initiator.defendRoll(receiver)
+      if (initiator.currentHp == 0) {
+        receiver.defeatEnemy(initiator)
+      }
+    }
+  }
   override def endCombat(): Unit = {
     context.setState(new MainLoop(context))
   }
